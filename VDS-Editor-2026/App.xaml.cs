bc6ad7e14bc6290c3de75877e755188145fc768d
koechlm <markus.koechl@autodesk.com>
2025-02-26 09:50:23 -0800

    Add project files.

---
+++ VDS-Editor-2026/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VDS_Editor_2026;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
